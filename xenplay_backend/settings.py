9995fae5757a167595129d41aa17665ab773a11e
Hemant Kumar <96202628+hemantkr1412@users.noreply.github.com>
2024-07-11 21:49:23 +0530

    Update settings.py

--- xenplay_backend/settings.py
+++ xenplay_backend/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -147,7 +148,7 @@ TIME_ZONE = 'Asia/Kolkata'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/v2")
 
 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
