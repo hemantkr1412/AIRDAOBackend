039ceb0552750b9eeab33d60ce5baa8bf67b5cb0
Hemant Kumar <96202628+hemantkr1412@users.noreply.github.com>
2024-07-12 10:39:55 +0530

    Update settings.py

--- xenplay_backend/settings.py
+++ xenplay_backend/settings.py
@@ -147,8 +147,11 @@ TIME_ZONE = 'Asia/Kolkata'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "api/v1/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = "api/v1/media/"
+STATIC_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
