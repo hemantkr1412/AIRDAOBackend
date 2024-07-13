da243312bd42141776e5e2a677faf019208d9588
Hemant Kumar <96202628+hemantkr1412@users.noreply.github.com>
2024-07-13 17:49:06 +0530

    Update settings.py

--- xenplay_backend/settings.py
+++ xenplay_backend/settings.py
@@ -151,10 +151,45 @@ TIME_ZONE = 'Asia/Kolkata'
 STATIC_URL = "api/v1/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-MEDIA_URL = "api/v1/media/"
-STATIC_ROOT = os.path.join(BASE_DIR, "media")
+
+if config["IS_LOCAL"]:
+    MEDIA_URL = "api/v1/media/"
+    STATIC_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+class StaticStorage(S3Boto3Storage):
+    location = "static"
+    default_acl = "public-read"
+
+
+class PublicMediaStorage(S3Boto3Storage):
+    location = "media2"
+    default_acl = "public-read"
+    file_overwrite = False
+
+
+if not config["IS_LOCAL"]:
+    AWS_ACCESS_KEY_ID = config["AWS_ACCESS_KEY_ID"]
+    AWS_SECRET_ACCESS_KEY = config["AWS_SECRET_ACCESS_KEY"]
+    AWS_STORAGE_BUCKET_NAME = config["AWS_STORAGE_BUCKET_NAME"]
+    AWS_S3_SIGNATURE_NAME = config["AWS_S3_SIGNATURE_NAME"]
+    AWS_S3_REGION_NAME = "ap-south-1"
+    AWS_S3_FILE_OVERWRITE = True
+    AWS_DEFAULT_ACL = None
+    AWS_S3_VERITY = True
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
+    AWS_LOCATION = "static"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    PUBLIC_MEDIA_LOCATION = "media2"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/"
+    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+
+
